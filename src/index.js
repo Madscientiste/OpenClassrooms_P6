922e80ae01a5104c2a05012b6319f8decc1d5d24
Madscientist <s.shaade77@gmail.com>
2021-06-21 07:25:49 +0200

    ayaya

--- src/index.js
+++ src/index.js
@@ -4,4 +4,10 @@ import "./styles/main.scss";
 import "./components/Stars";
 import "./components/MoviePoster"
 
+const carousel = document.querySelector(".carousel");
+const controller = carousel.querySelector(".carousel-controller");
+const body = carousel.querySelector(".carousel-body");
+
+
+
 if (module.hot) module.hot.accept();
