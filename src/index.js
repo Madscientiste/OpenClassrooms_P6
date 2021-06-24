66c5dd07e95d518f39bd1dbec1cd76511f39ef38
Madscientist <s.shaade77@gmail.com>
2021-06-24 21:46:55 +0200

    refactoing v999999

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 import "./styles/main.scss";
+import api from "./utilities/api"
 
-// components
-import "./components/Stars";
-import "./components/MoviePoster"
-
-const carousel = document.querySelector(".carousel");
-const controller = carousel.querySelector(".carousel-controller");
-const body = carousel.querySelector(".carousel-body");
-
+const movies = api.getTitles({ sort_by: "-imdb_score" })
+movies.then((e) => {
+    console.log(e.data.results)
+})
 
 
 if (module.hot) module.hot.accept();
